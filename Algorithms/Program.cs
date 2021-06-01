a6a3e0586ee51b4c8c91a1d5b1c37c6b63c4a062
Bill A <this@that.com>
2021-05-31 20:43:30 -0400

    * Program.cs: Integrated Sorted Square Array
    * AlgoSortedSquareArray.cs: Added Sorted Square Array

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -10,9 +10,14 @@ namespace Algorithms
         static void Main(string[] args)
         {
             //int[] ssaTest = new int[] { 1, 2, 3, 5, 6, 8, 9 };
-            int[] ssaTest = new int[] { -2, -1 };
-            var ssaResults = AlgoSortedSquareArray.SortedSquareArray(ssaTest);
+            //int[] ssaTest = new int[] { -2, -1 };
+            int[] ssaTest = new int[] { -10, -5, 0, 5, 10 };
+            //int[] ssaTest = new int[] { -7, -5, -4, 3, 6, 8, 9 };
+            //var ssaResults = AlgoSortedSquareArray.SortedSquareArray(ssaTest);
+            var ssaResults = AlgoSortedSquareArray.SortedSquareArray2(ssaTest);
             Console.WriteLine($"{string.Join(", ", ssaResults)}");
+
+
 
             //string tournamentWinner = 
             //    AlgoTournamentWinner.TournamentWinner(AlgoTournamentWinner.competitions, 
--- Algorithms/SortedSquaredArray/AlgoSortedSquareArray.cs
+++ Algorithms/SortedSquaredArray/AlgoSortedSquareArray.cs
@@ -23,5 +23,33 @@ namespace Algorithms.SortedSquaredArray
 
             return squares;
         }
+
+        public static int[] SortedSquareArray2(int[] array)
+        {
+            int[] squares = new int[array.Length];
+            int i = squares.Length - 1;
+            int lowest = 0;
+            int highest = i;
+
+            while (lowest != highest)
+            {
+                if (Math.Abs(array[lowest]) > Math.Abs(array[highest]))
+                {
+                    squares[i] = (int)Math.Pow(array[lowest], 2);
+                    lowest++;
+                }
+                else
+                {
+                    // Values are equal or above condition not true so the
+                    // value to insert has to be the higher value
+                    squares[i] = (int)Math.Pow(array[highest], 2);
+                    highest--;
+                }
+                i--;
+            }
+            squares[i] = (int)Math.Pow(array[highest], 2);
+
+            return squares;
+        }
     }
 }
